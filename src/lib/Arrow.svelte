5aea710691898de92435e5d880e122277434bc73
Script Raccoon <scriptraccoon@gmail.com>
2024-12-27 16:21:02 +0100

    preview drawing line

---
+++ src/lib/Arrow.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	type Props = {
+		start: { x: number; y: number };
+		end: { x: number; y: number };
+	};
+
+	const { start, end }: Props = $props();
+
+	let length = $derived(
+		Math.sqrt((end.x - start.x) ** 2 + (end.y - start.y) ** 2)
+	);
+
+	let angle = $derived(
+		(180 / Math.PI) * Math.atan2(end.y - start.y, end.x - start.x)
+	);
+</script>
+
+<div
+	class="arrow"
+	style:--pos_x="{start.x}px"
+	style:--pos_y="{start.y}px"
+	style:--length="{length}px"
+	style:--angle="{angle}deg"
+></div>
+
+<style>
+	.arrow {
+		--thickness: 4px;
+		position: absolute;
+		transform-origin: left;
+		top: var(--pos_y);
+		left: var(--pos_x);
+		width: var(--length);
+		height: var(--thickness);
+		translate: 0px calc(-0.5 * var(--thickness));
+		rotate: var(--angle);
+		background-color: orange;
+	}
+</style>
--- src/lib/Grid.svelte
+++ src/lib/Grid.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
+	import Arrow from './Arrow.svelte';
 	import { step } from './state.svelte';
 
 	let size = $state({ x: 1, y: 1 });
-	const padding = 25;
-
-	let nodes = $state<string[]>([]);
-	let arrows = $state<string[]>([]);
-
-	let start_node = $state<string | null>(null);
-
-	const k = (x: number, y: number) => `${x},${y}`;
 
 	$effect(() => {
 		size.x = Math.floor((window.innerWidth - 2 * padding) / 100);
 		size.y = Math.floor((window.innerHeight - 2 * padding) / 100);
 	});
 
-	function handle_node_click(x: number, y: number) {
-		const node = k(x, y);
+	const padding = 25;
 
+	let nodes = $state<string[]>([]);
+	let arrows = $state<string[]>([]);
+
+	let start_node = $state<{ x: number; y: number } | null>(null);
+
+	let grid_element = $state<HTMLElement | null>(null);
+
+	const k = (x: number, y: number) => `${x},${y}`;
+
+	let mouse_pos = $state<{ x: number; y: number }>({ x: 0, y: 0 });
+
+	$effect(() => {
+		window.addEventListener('mousemove', update_mouse_pos);
+		return () => window.removeEventListener('mousemove', update_mouse_pos);
+	});
+
+	function update_mouse_pos(e: MouseEvent) {
+		if (!grid_element) return;
+		const rect = grid_element.getBoundingClientRect();
+		mouse_pos.x = e.clientX - rect.left;
+		mouse_pos.y = e.clientY - rect.top;
+	}
+
+	function handle_node_click(x: number, y: number) {
 		if (step.value === 1) {
-			if (nodes.includes(node)) {
-				nodes = nodes.filter((_node) => _node != node);
+			const key = k(x, y);
+			if (nodes.includes(key)) {
+				nodes = nodes.filter((_node) => _node != key);
 			} else {
 				nodes.push(k(x, y));
 			}
 		} else if (step.value === 2) {
+			const node = { x, y };
 			if (start_node) {
 				if (start_node !== node) {
 					const arrow = `${start_node}->${node}`;
@@ -42,7 +60,12 @@
 </script>
 
 <div class="grid-wrapper step-{step.value}">
-	<div class="grid" style:--x={size.x} style:--y={size.y}>
+	<div
+		class="grid"
+		style:--x={size.x}
+		style:--y={size.y}
+		bind:this={grid_element}
+	>
 		{#each { length: size.y } as _, y}
 			{#each { length: size.x } as _, x}
 				{@const selected = nodes.includes(k(x, y))}
@@ -59,6 +82,15 @@
 				</span>
 			{/each}
 		{/each}
+		{#if start_node && grid_element}
+			<Arrow
+				start={{
+					x: start_node.x * 100,
+					y: start_node.y * 100
+				}}
+				end={mouse_pos}
+			/>
+		{/if}
 	</div>
 </div>
 
@@ -76,6 +108,7 @@
 		grid-template-columns: repeat(var(--x), 100px);
 		grid-template-rows: repeat(var(--y), 100px);
 		outline: 1px solid #333;
+		position: relative;
 	}
 
 	.tile {
