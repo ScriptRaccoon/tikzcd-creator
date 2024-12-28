4a0fc713c093e30bb71b885df7c9c748da7d2afb
Script Raccoon <scriptraccoon@gmail.com>
2024-12-28 06:26:31 +0100

    remove unused option

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,7 @@ export function render_latex(input: string): string {
 	// @ts-ignore
 	if (typeof MathJax !== undefined) {
 		// @ts-ignore
-		return MathJax.tex2svg(input, { em: 12, ex: 6, display: false }).outerHTML
+		return MathJax.tex2svg(input, { display: false }).outerHTML
 	}
 	return ''
 }
