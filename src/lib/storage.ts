ee4570e843b86f1f8c9a158485008e0d22c6140b
Script Raccoon <scriptraccoon@gmail.com>
2024-12-29 17:12:05 +0100

    sync data with localstorage

---
+++ src/lib/storage.ts
@@ -0,0 +1,38 @@
+import { browser } from '$app/environment'
+import type { StepIndex } from './step.config'
+import type { Diagram } from './types'
+
+const storage_key = 'data'
+
+type StorageData = {
+	diagram: Diagram
+	step: StepIndex
+}
+
+const default_data: StorageData = {
+	diagram: { nodes: [], arrows: [] },
+	step: 0
+}
+
+export function get_stored_data(): StorageData {
+	if (!browser) return default_data
+
+	try {
+		const data = localStorage.getItem(storage_key)
+		if (!data) return default_data
+		return JSON.parse(data)
+	} catch (err) {
+		console.error(err)
+		return default_data
+	}
+}
+
+export function save_data(data: { diagram: Diagram; step: StepIndex }) {
+	if (!browser) return
+
+	try {
+		localStorage.setItem(storage_key, JSON.stringify(data))
+	} catch (err) {
+		console.error(err)
+	}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,25 @@
 	import CodeDisplay from '$lib/components/CodeDisplay.svelte'
 	import Grid from '$lib/components/Grid.svelte'
 	import Step from '$lib/components/Step.svelte'
+
 	import type { StepIndex } from '$lib/step.config'
+	import { get_stored_data, save_data } from '$lib/storage'
 	import { type Diagram } from '$lib/types'
 
-	let diagram = $state<Diagram>({ nodes: [], arrows: [] })
-	let step = $state<StepIndex>(0)
+	const initial_data = get_stored_data()
+
+	let diagram = $state<Diagram>(initial_data.diagram)
+	let step = $state<StepIndex>(initial_data.step)
 
 	function reset() {
 		diagram.nodes = []
 		diagram.arrows = []
 		step = 1
 	}
+
+	$effect(() => {
+		save_data({ diagram, step })
+	})
 </script>
 
 <Grid bind:diagram {step} />
