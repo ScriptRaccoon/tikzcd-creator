eed1c62f055bf9b83139cd2ac3460e13754675b5
Script Raccoon <scriptraccoon@gmail.com>
2024-12-30 23:41:11 +0100

    smaller tile size on small screens

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,11 @@
-export const tile_size = 100
+import { browser } from '$app/environment'
+
+const get_tile_size = () => {
+	if (!browser) return 100
+	return window.innerWidth > 900 ? 100 : 50
+}
+
+export const tile_size = get_tile_size()
 export const grid_padding = 25
 export const arrow_padding = 30
 export const arrow_tip_size = 20
