630fb9aa9cb4735ba9b861c8341afdb3050df291
Script Raccoon <scriptraccoon@gmail.com>
2025-01-11 12:08:24 +0100

    generate url for diagram

---
+++ src/lib/url.ts
@@ -0,0 +1,40 @@
+import { browser } from '$app/environment'
+
+import { decode_diagram, encode_diagram } from './encoding'
+import { STEPS } from './step.config'
+import type { StorageData } from './types'
+
+export function generate_URL(data: StorageData): string {
+	if (!browser) return ''
+
+	const encoded_diagram = encode_diagram(data.diagram)
+
+	const url = new URL(window.location.href)
+	url.searchParams.set('d', encoded_diagram)
+	url.searchParams.set('s', data.step.toString())
+	url.searchParams.set('t', data.tile_size.toString())
+
+	return url.toString()
+}
+
+export function get_data_from_URL(): StorageData | null {
+	if (!browser) return null
+
+	const url = new URL(window.location.href)
+	const diagram_parameter = url.searchParams.get('d')
+	const step_parameter = url.searchParams.get('s')
+	const tile_size_parameter = url.searchParams.get('t')
+
+	if (!diagram_parameter || !step_parameter || !tile_size_parameter) return null
+
+	const diagram = decode_diagram(diagram_parameter)
+	if (!diagram) return null
+
+	const step = parseInt(step_parameter)
+	if (Number.isNaN(step) || step < 0 || step >= STEPS.length) return null
+
+	const tile_size = parseInt(tile_size_parameter)
+	if (Number.isNaN(tile_size) || tile_size < 1) return null
+
+	return { diagram, step, tile_size }
+}
